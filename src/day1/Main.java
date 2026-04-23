da36df349dbeb3ad0a073a98082f6f59955d2f4e
Manoj Kate <manojkate9696@email.com>
2026-04-23 08:20:54 +0530

    Added .gitignore to ignore bin files

---
+++ src/day1/Main.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Main {
+     public static void main(String[] args) {
+    	 Student s1 = new Student();
+    	 s1.name = "Manoj";
+    	 s1.age = 22;
+    	 s1.display();
+     }
+}
---
+++ src/day1/Student.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Student {
+   String name;
+   int age;
+   Student(){
+	   System.out.println("Hello Students");
+   }
+   void display() {
+	   System.out.println("Name of the Student: "+name);
+	   System.out.println("Age of the Student: "+age);
+   }
+}
